f763e72db884207c3812c98fbfecfa365cd9163d
Jan Vrba <jvhona@gmail.com>
2024-10-03 10:23:12 +0200

    funkce obsah_obdelniku

---
+++ obsah.py
@@ -0,0 +1,11 @@
+def obsah_obdelniku():
+    a = int(input("Zadej velikost strany a v cm: "))
+    b = int(input("Zadej velikost strany b v cm: "))
+    if a > 0 and b > 0:
+        obsah = a * b
+        print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
+    else:
+        print("Špatné hodnoty. Zkuste znovu")
+        obsah_obdelniku()
+
+obsah_obdelniku()
